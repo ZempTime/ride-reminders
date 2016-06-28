def8561a77dd14e30c954c48ad58388aad75db9c
Chris Zempel <zempel@efeqdev.com>
2016-06-28 14:56:58 -0500

    largely functional new alarm form

--- app/helpers/alarms_helper.rb
+++ app/helpers/alarms_helper.rb
@@ -1,5 +1,13 @@
 module AlarmsHelper
   def weekdays
-    weekdays = Date::DAYNAMES.rotate(1) # makes Monday first day of week
+    weekdays = Date::DAYNAMES.rotate(1).each_with_index.map { |d, i| Weekday.new(d, i) } # makes Monday first day of week
+  end
+
+  class Weekday
+    attr_accessor :name, :index
+    def initialize(name, index)
+      @name = name
+      @index = index
+    end
   end
 end
