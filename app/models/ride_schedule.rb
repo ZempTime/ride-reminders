cc4f1cbc8d5c553cb5341d6fd66e23f2986b06f6
Chris Zempel <zempel@efeqdev.com>
2016-07-03 21:05:03 -0500

    Adds validations, sets up relationships with Ride Schedule and Travel Method

--- app/models/ride_schedule.rb
+++ app/models/ride_schedule.rb
@@ -1,4 +1,6 @@
 class RideSchedule < ApplicationRecord
   belongs_to :travel_method
   has_one :week, dependent: :destroy
+
+  validates :starts_at, :interval_minutes, presence: true
 end
--- app/models/travel_method.rb
+++ app/models/travel_method.rb
@@ -1,3 +1,5 @@
 class TravelMethod < ApplicationRecord
+  has_many :ride_schedules
+
   validates_presence_of :name, :description
 end
