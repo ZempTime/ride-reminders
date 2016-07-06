d003ad50691f1d5b5fbc728d948a3831f83b53a6
Chris Zempel <zempel@efeqdev.com>
2016-07-05 21:49:28 -0500

    Adds reminders to routes, nested under alarms

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "accounts/sessions", registrations: "accounts/registrations", passwords: "accounts/passwords" }
 
-  resources :alarms
+  resources :alarms do
+    resources :reminders
+  end
+
   resources :ride_schedules
 
   root to: "ride_schedules#index"
