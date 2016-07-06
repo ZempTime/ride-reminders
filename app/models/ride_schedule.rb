20cf062ed64fb324bda7b4b3bf55e80fddf72d82
Chris Zempel <zempel@efeqdev.com>
2016-07-05 20:24:31 -0500

    Adds ride schedules show

--- app/models/ride_schedule.rb
+++ app/models/ride_schedule.rb
@@ -7,4 +7,12 @@ class RideSchedule < ApplicationRecord
   validates_presence_of :week
 
   accepts_nested_attributes_for :week, allow_destroy: true, reject_if: :all_blank
+
+  def title
+    "#{travel_method.name} - #{abbreviated_weekdays}"
+  end
+
+  def abbreviated_weekdays
+    week.abbreviated_weekdays
+  end
 end
--- app/models/week.rb
+++ app/models/week.rb
@@ -1,3 +1,23 @@
 class Week < ApplicationRecord
   belongs_to :owner, polymorphic: true, required: false
+
+  def abbreviated_weekdays
+    result = ""
+    result += "S" if sunday
+    result += "M" if monday
+    result += "T" if tuesday
+    result += "W" if wednesday
+    result += "H" if thursday
+    result += "F" if friday
+    result += "S" if saturday
+
+    case result
+    when "MTWHF"
+      return "Weekdays"
+    when "SS"
+      return "Weekends"
+    else
+      result
+    end
+  end
 end
